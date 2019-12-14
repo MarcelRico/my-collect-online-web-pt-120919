acbb05a8d075233297d88624719e900fc9a7119e
Marcel Rico <personal.mr4@gmail.com>
2019-12-14 13:04:23 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect(array)
+  if block_given? # Check if block is provided to method
+    idx = 0 # Initialize Counter
+    collection = [] # Initialize Final Collection Array
+    while array[idx]
+      collection << yield(array[idx])
+    end
+  end
+  collection
+end
