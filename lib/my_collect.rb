79f21c1c36e613c1731870f358b5c20e3a49b4fc
Marcel Rico <personal.mr4@gmail.com>
2019-12-14 13:05:17 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,12 @@
 def my_collect(array)
+  
   if block_given? # Check if block is provided to method
     idx = 0 # Initialize Counter
     collection = [] # Initialize Final Collection Array
-    while array[idx]
+    
+    while array[idx] # Load collection array with results from yield
       collection << yield(array[idx])
     end
   end
-  collection
+  collection # Return Final Collection Array
 end
